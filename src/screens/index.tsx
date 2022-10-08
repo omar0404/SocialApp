7ef6cb74cc19f288f74f114eee41241936560d40
omar0404 <omar97.hassan97@gmail.com>
2022-10-08 10:26:09 +0200

    style : update eslint rules/prettier and  apply them

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,2 +1,3 @@
 import Home from './Home';
-export { Home };
+import PostDetails from './PostDetails';
+export { PostDetails, Home };
--- src/service/SensitiveInfo.ts
+++ src/service/SensitiveInfo.ts
@@ -8,6 +8,6 @@ const getItem = async (key: string): Promise<string> =>
   await SInfo.getItem(key, OPTIONS);
 const setItem = async (key: string, value: string): Promise<null> =>
   await SInfo.setItem(key, value, OPTIONS);
-const SensitiveInfo = {getItem, setItem};
+const SensitiveInfo = { getItem, setItem };
 
-export {SensitiveInfo};
+export { SensitiveInfo };
